a563c408e1b34dbdc73ea438a5eb824cf2213f59
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-07-23 17:41:23 +0530

    Completed Step 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [userInputState, setUserInputState] = useState({
+    userInput: '',
+  });
+
+  const inputChangeHandler = (event) => {
+    setUserInputState({
+      userInput: event.target.value
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <input 
+      type='text' 
+      onChange={inputChangeHandler}
+      value={userInputState.userInput} />
+
+      <p> {userInputState.userInput} </p>
     </div>
   );
 }
