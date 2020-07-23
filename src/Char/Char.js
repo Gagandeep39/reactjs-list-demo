373f63c530d47cdfd8c5ef695356dc2ff02d2294
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-07-23 17:54:47 +0530

    Cmpleted step 4

---
+++ src/Char/Char.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const char = (props) => {
+  const style = {
+    display: 'inline-bock',
+    padding: '16px',
+    margin: '16px',
+    border: '1px solid bloack',
+    teztAlign: 'center'
+  }
+  return (
+    <div style={style}>
+
+    </div>
+  );
+}
+
+export default char;
